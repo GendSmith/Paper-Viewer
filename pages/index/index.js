56d62520a686c68aed62a6293669848bd998f081
dengzhongtian <1439786496@qq.com>
2017-12-06 22:05:03 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+Page(
+{
+    data:{
+      list:[
+        {
+          id:'politics',
+          name:'政治',
+          url:'pages/index/politics'
+        },
+        {
+          id:'economic',
+          name:'经济',
+          url:'pages/index/economic'
+        }
+      ]
+
+    }
+})
